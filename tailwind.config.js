deb2c64208f109450ea171447ac14cc6f9848ee1
Prashant <prashantamoli2621@gmail.com>
2024-01-09 12:43:35 +0530

    tailwindcss added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  darkMode: "class",
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
