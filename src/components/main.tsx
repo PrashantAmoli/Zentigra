471a17cbeacb948aa483f015345778139a249570
Prashant <prashantamoli2621@gmail.com>
2024-01-22 13:14:33 +0530

    shadcn-ui init

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,7 +1,5 @@
-
 import { useUser } from "@clerk/clerk-react"
-import { useState } from "react"
-
+import { useEffect, useState } from "react"
 
 type UserData = {
   emailAddresses: unknown[]
@@ -10,37 +8,39 @@ type UserData = {
 }
 
 export function Main({ name = "Extension" }) {
+  // const userData = useUser()
 
-  const userData = useUser()
+  useEffect(() => {
+    chrome.runtime?.onMessage?.addListener(
+      function (message, sender, sendResponse) {
+        if (message.action === "backgroundToPopup") {
+          console.log("Message from background script:", message.data)
 
-  const changeState = (newState) =>{
+          // Do the DB call here
+        }
+      }
+    )
+  }, [])
+
+  const changeState = (newState) => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      console.log('inside the tab function')
-      const activeTabId = tabs[0].id;
-      chrome.tabs.sendMessage(activeTabId, { action: newState });
-    });
+      console.log("inside the tab function")
+      const activeTabId = tabs[0].id
+      chrome.tabs.sendMessage(activeTabId, { action: newState })
+    })
   }
-
-
-  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action === "backgroundToPopup") {
-      console.log("Message from background script:", message.data);
-  
-      // Do the DB call here
-    }
-  });
-
 
   return (
     <div>
       <div id="id1">-</div>
-      <button onClick={()=>changeState('start')}>Start</button>
+      <button onClick={() => changeState("start")}>Start</button>
       {/* <button onClick={()=>changeState('pause')}>Pause</button> */}
-      <button onClick={()=>changeState('stop')}>Stop</button>
-      
+      <button onClick={() => changeState("stop")}>Stop</button>
+
       <div className="w-full h-80 flex justify-center items-center">
         <h1 className="text-center text-5xl font-bold">
-          Welcome to your Zentigra {userData.user?.firstName}!
+          Welcome to your Zentigra
+          {/* {userData.user?.firstName}! */}
         </h1>
       </div>
     </div>
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,7 +1,13 @@
 import { Main } from "~components/main"
 
 function IndexPopup() {
-  return <Main />
+  return (
+    <>
+      <main className="min-w-96 h-96 flex justify-center items-center">
+        <Main />
+      </main>
+    </>
+  )
 }
 
 export default IndexPopup
