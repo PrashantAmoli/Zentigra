2d741fb81d74a808e2ec78e9f91042f9253cc69b
Prashant <prashantamoli2621@gmail.com>
2024-02-15 01:15:34 +0530

    first upload via app

--- src/background/index.ts
+++ src/background/index.ts
@@ -7,25 +7,8 @@ console.log(
   "Live now; make now always the most precious time. Now will never come again."
 )
 
-const openPopup = () => {
-  chrome.tabs.create({
-    url: chrome.runtime.getURL("/popup.html")
-  })
-}
-
-const openPreviewPage = () => {
-  chrome.tabs.create({
-    url: frontendUrl
-      ? `${frontendUrl}/preview`
-      : "http://localhost:1947/preview"
-  })
-}
-
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === "contentScriptToPopup") {
-    // openPopup()
-    openPreviewPage()
-
     // Log the message received from the content script
     console.log("Message from content script:", message.command, message.data)
 
@@ -37,35 +20,47 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
       //   command: message.command
       // })
       // Send the message to the preview page
-      // chrome.tabs.query(
-      //   {
-      //     url: frontendUrl
-      //       ? `${frontendUrl}/preview`
-      //       : "http://localhost:1947/preview",
-      //     currentWindow: true
-      //     // active: true
-      //   },
-      //   (tabDetails) => {
-      //     console.log("Preview Tabs: ", tabDetails)
-      //     //sends messages to all active tabs for now
-      //     tabDetails.forEach((tab) => {
-      //       chrome.tabs.sendMessage(tab.id, {
-      //         action: "backgroundToPreview",
-      //         command: "all-tabs",
-      //         data: message.data
-      //       })
-      //     })
-      //   }
-      // )
-      // window.postMessage(
-      //   {
-      //     action: "backgroundToPreview",
-      //     data: message.data,
-      //     command: message.command
-      //   },
-      //   "*"
-      // )
     }, 5000)
+  } else if (message.action === "contentScriptToPreview") {
+    chrome.tabs
+      .create({
+        url: frontendUrl
+          ? `${frontendUrl}/preview`
+          : "http://localhost:1947/preview"
+      })
+      .then((tab) => {
+        console.log("Preview Tab: ", tab.id)
+
+        setTimeout(() => {
+          // send only to the newly created preview tab
+          chrome.tabs.sendMessage(tab.id, {
+            action: "backgroundToPreview",
+            command: "all-tabs",
+            data: message.data
+          })
+        }, 9000)
+
+        // send to all preview tabs
+        // chrome.tabs.query(
+        //   {
+        //     url: frontendUrl
+        //       ? `${frontendUrl}/preview`
+        //       : "http://localhost:1947/preview"
+        //   },
+        //   (tabDetails) => {
+        //     console.log("Preview Tabs: ", tabDetails)
+
+        //     //sends messages to all active tabs for now
+        //     tabDetails.forEach((tab) => {
+        //       chrome.tabs.sendMessage(tab.id, {
+        //         action: "backgroundToPreview",
+        //         command: "all-tabs",
+        //         data: message.data
+        //       })
+        //     })
+        //   }
+        // )
+      })
   }
 })
 
@@ -77,7 +72,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
       url: frontendUrl ? `${frontendUrl}` : "https://zentigra.com"
     })
   } else if (details.reason === "update") {
-    chrome.tabs.query({ active: true }, (tabDetails) => {
+    chrome.tabs.query({ active: false }, (tabDetails) => {
       //sends messages to all active tabs for now
       tabDetails.forEach((tab) => {
         chrome.tabs.sendMessage(tab.id, {
--- src/contents/content.ts
+++ src/contents/content.ts
@@ -36,44 +36,9 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
       // TODO: send the data to the Zentigra app
       // Open Zentigra preview page & send the sequenceData to it
-
-      // renderZentigraIFrame()
-
-      setTimeout(() => {
-        // const zentigraIframe = document.getElementById(
-        //   "zentigra-iframe"
-        // ) as HTMLIFrameElement
-        // zentigraIframe.contentWindow.postMessage(
-        //   {
-        //     action: "contentScriptToPreview",
-        //     command: "stop",
-        //     data: sequenceData
-        //   },
-        //   "*"
-        // )
-
-        window.postMessage(
-          {
-            action: "contentScriptToPreview",
-            command: "stop",
-            data: [...sequenceData]
-          },
-          "*"
-        )
-      }, 20000)
-
-      // open the preview page in a new tab
-      // window
-      //   .open(
-      //     "http://localhost:1947/preview",
-      //     "_blank",
-      //     "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400"
-      //   )
-      //   .focus()
-
       // send to background
       chrome.runtime.sendMessage({
-        action: "contentScriptToPopup",
+        action: "contentScriptToPreview",
         command: "stop",
         data: sequenceData
       })
@@ -86,46 +51,19 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   }
 })
 
-//TEMPORARY
-const renderImage = (src) => {
-  const imgElement = document.createElement("img")
-  imgElement.src = src
-  imgElement.alt = "Description of the image"
-  imgElement.width = 800
-  document.body.appendChild(imgElement)
-}
-
-const captureScreen = () => {
-  const ss = htmlScreenCaptureJs.capture(
-    htmlScreenCaptureJs.OutputType.STRING,
-    document
-  )
-
-  console.log("ss: ", `data:text/html;charset=utf-8,${encodeURI(`${ss}`)}`)
-}
-
 document.addEventListener("mousedown", function (event) {
   const x = event.clientX
   const y = event.clientY
   console.log("Mouse clicked at:", { x: event.clientX, y: event.clientY })
 
-  // captureScreen()
-
   // TODO: send sample data to the preview page
-  if (window.location.href.includes("localhost:1947")) {
-    window.postMessage(
-      {
-        action: "contentScriptToPreview",
-        command: "stop",
-        data: { x, y }
-      },
-      `*`
-    )
-  }
+  // chrome.runtime.sendMessage({
+  //   action: "contentScriptToPreview",
+  //   command: "start",
+  //   data: { x, y }
+  // })
 
-  if (currentState !== "start") {
-    return
-  }
+  if (currentState !== "start") return
 
   const screenshotTarget = document.body
 
@@ -137,28 +75,18 @@ document.addEventListener("mousedown", function (event) {
       canvasHeight: window.innerHeight
     })
     .then(function (canvas) {
-      // document.body.appendChild(canvas);
-
       const base64image = canvas.toDataURL("image/png")
       console.log("this is the image: ", base64image)
 
-      window.postMessage(
-        {
-          action: "contentScriptToPreview",
-          command: "stop",
-          data: { url: base64image }
-        },
-        `${frontendUrl}/preview/`
-      )
-
       chrome.storage.local.get(["image"]).then((result) => {
-        console.log("Value currently is " + result["image"])
+        // console.log("Value currently is " + result["image"])
         let imageString = result["image"]
 
-        console.log("this is the imageString: ", imageString)
+        // console.log("this is the imageString: ", imageString)
         if (imageString == null) {
           imageString = "[]"
         }
+
         let imageArray = JSON.parse(imageString)
         console.log("imageArray: ", imageArray)
         imageArray.push({ url: base64image, x, y })
@@ -172,38 +100,25 @@ document.addEventListener("mousedown", function (event) {
     })
 })
 
-const renderZentigraIFrame = () => {
-  const iframe = document.createElement("iframe")
-  iframe.src = frontendUrl
-    ? `${frontendUrl}/preview`
-    : "http://localhost:1947/preview"
-  iframe.width = "100%"
-  iframe.height = "100%"
-  iframe.id = "zentigra-iframe"
+// window.onmessage = (event) => {
+//   console.log("onmessage in CS: ", event.data)
 
-  iframe.setAttribute(
-    "allow",
-    "camera; microphone; fullscreen; display-capture; autoplay; encrypted-media; picture-in-picture; "
-  )
-  iframe.setAttribute("allowfullscreen", "true")
-  iframe.setAttribute("frameborder", "0")
+//   if (event.data.command === "received-sequence") {
+//     console.log("Next=>CS: ", event.data)
+//   }
+// }
 
-  iframe.style.position = "fixed"
-  iframe.style.top = "0"
-  iframe.style.bottom = "0"
-  iframe.style.left = "0"
-  iframe.style.right = "0"
-  iframe.style.zIndex = "999999"
-  iframe.style.border = "none"
-  iframe.style.width = "100%"
-  iframe.style.height = "100vh"
-  iframe.style.overflowY = "scroll"
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log("Message from background: ", message)
 
-  document.body.appendChild(iframe)
-}
-
-window.onmessage = (event) => {
-  if (event.data.command === "received-sequence") {
-    console.log("Next=>CS: ", event.data)
+  if (message.action === "backgroundToPreview") {
+    window.postMessage(
+      {
+        action: "backgroundToPreview",
+        command: "stop",
+        data: message.data
+      },
+      "*"
+    )
   }
-}
+})
